2255fe66d4111e9adb9117bd743e7ef0e5788c49
Dhruva Vaibhav Sambhushete <vaibhav.sambhushete@dacpl.in>
2025-03-27 16:14:24 +0530

    Add Variant Functionality

---
+++ BoostrapTemplate/ViewModels/VarraintCode/VarraintCode_Edit_ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OMS_Template.ViewModels.VarraintCode
+{
+    public class VarraintCode_Edit_ViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Enter Erp Vcode")]
+        [Display(Name = "Erp Vcode")]
+        public string Erp_Vcode { get; set; }
+        [Required(ErrorMessage = "Enter Description")]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Enter Mes Vcode")]
+        [Display(Name = "Mes Vcode")]
+        //[RegularExpression(@"^\d{5}$", ErrorMessage = "The number must be exactly 5 digits.")]
+        [Range(0, 99999, ErrorMessage = "Wrong MES Vcode")]
+        public int Mes_Vcode { get; set; }
+    }
+}
---
+++ BoostrapTemplate/ViewModels/VarraintCode/VarraintCode_List_ViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace OMS_Template.ViewModels.VarraintCode
+{
+    public class VarraintCode_List_ViewModel
+    {
+        public IEnumerable<VarraintCode_Add_ViewModel> VcList { get; set; }         
+    }
+}
